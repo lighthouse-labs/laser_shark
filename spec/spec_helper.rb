96355e93ad73b12642473ea08a66619e0f4564e7
Khurram Virani <kvirani@gmail.com>
2014-04-03 13:33:51 -0700

    removed factory girl require

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,6 @@ require 'database_cleaner'
 require 'capybara/rspec'
 require 'capybara/rails'
 require 'capybara/poltergeist'
-require 'factory_girl_rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
