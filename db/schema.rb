bd2ceccc3f915583778fe51e40bac8ca8649a6ff
matrinox <geofflee21@me.com>
2014-04-03 03:09:42 -0700

    Add uid/token fields to student model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140403025300) do
+ActiveRecord::Schema.define(version: 20140403042713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 20140403025300) do
     t.string "phone_number"
     t.string "twitter"
     t.string "skype"
+    t.string "uid"
+    t.string "token"
   end
 
 end
