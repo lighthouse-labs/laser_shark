41a31553106b7c6ae4198731fddf30761afe860e
matrinox <geofflee21@me.com>
2014-04-04 13:34:18 -0700

    Allow home spec to run logged in

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -5,6 +5,7 @@ require 'spec_helper'
 describe 'Home page' do
 
   it 'works (to prove that poltergeist and phantomjs based integration tests are working) - REMOVE THIS TEST LATER', js: true do
+  	allow(Student).to receive(:find_by_id).and_return Student.new
     visit root_path
     # page.save_screenshot('home.png')
     expect(page).to have_content('Home')
