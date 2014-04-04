ff159fd027878ccf0a49846c8e80f8bfcedf64b2
matrinox <geofflee21@me.com>
2014-04-03 22:35:05 -0700

    Redirects to setting up new student if needed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,14 @@
 class SessionsController < ApplicationController
+	skip_before_action :authenticate, only: [:create]
+
 	def create
 		@current_student = Student.find_by_auth_hash(auth_hash_params)
-		redirect_to root_url
+		session[:student_id] = @current_student.id
+		if @current_student.needs_setup?
+			redirect_to new_student_path
+		else
+			redirect_to root_url
+		end
 	end
 
 	protected
