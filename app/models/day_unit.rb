0d295becc5f9a491b5a1f61176ebcf94f0e40ea1
Melissa Hubert <meloizabeth@live.com>
2014-04-16 22:57:28 +0000

    fixed indendation

--- app/models/day_unit.rb
+++ app/models/day_unit.rb
@@ -2,6 +2,6 @@ class DayUnit < ActiveRecord::Base
 
 	belongs_to :cohort
 	validates :name, presence: true, length: { maximum: 56 }
-	validates :day, numericality: { only_integer:true, minimum: 1 }
+	validates :day, numericality: { only_integer: true, minimum: 1 }
 
 end
