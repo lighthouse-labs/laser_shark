8b460dd3be7f43856d5c6203c78cc55cbbe2e2e1
Melissa Hubert <meloizabeth@live.com>
2014-04-16 23:45:39 +0000

    indentation

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,5 +1,5 @@
 class Cohort < ActiveRecord::Base
-	has_many :day_units
+  has_many :day_units
   has_many :students
   validates :name, presence: true
 end
--- spec/models/day_unit_spec.rb
+++ spec/models/day_unit_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe DayUnit do
 
-	it "has a valid factory" do
+  it "has a valid factory" do
     expect(build(:day_unit)).to be_valid
-	end
+  end
 
   it "should be invalid without name" do
     day_unit = build(:day_unit, name: nil)
