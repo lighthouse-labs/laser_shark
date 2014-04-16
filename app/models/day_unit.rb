574c1df27a73a15eda13d4a9d03a2965a1cd226f
Melissa Hubert <meloizabeth@live.com>
2014-04-16 23:50:01 +0000

    indentation

--- app/models/day_unit.rb
+++ app/models/day_unit.rb
@@ -1,7 +1,7 @@
 class DayUnit < ActiveRecord::Base
-
-	belongs_to :cohort
-	validates :name, presence: true, length: { maximum: 56 }
-	validates :day, numericality: { only_integer: true, minimum: 1 }
+  
+  belongs_to :cohort
+  validates :name, presence: true, length: { maximum: 56 }
+  validates :day, numericality: { only_integer: true, minimum: 1 }
 
 end
