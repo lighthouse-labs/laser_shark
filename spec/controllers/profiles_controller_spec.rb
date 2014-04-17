23a710c24a361552add8da96487ace30db34c471
matrinox <geofflee21@me.com>
2014-04-17 00:47:26 -0700

    Remove redundant attributes

--- spec/controllers/profiles_controller_spec.rb
+++ spec/controllers/profiles_controller_spec.rb
@@ -17,30 +17,30 @@ describe ProfilesController do
 
   describe "PATCH #update" do
     it "assigns the registration form to @form" do
-      patch :update, student: attributes_for(:student, uid: current_student.uid, token: current_student.token)
+      patch :update, student: attributes_for(:student)
       expect(assigns(:form)).to be_a(RegistrationForm)
     end
     context "with valid attributes" do
       it "does save the attributes to student" do
         expect do
-          patch :update, student: attributes_for(:student, first_name: "new", uid: current_student.uid, token: current_student.token)
+          patch :update, student: attributes_for(:student, first_name: "new")
           current_student.reload
         end.to change(current_student, :first_name)
       end
       it "redirects to root url" do
-        patch :update, student: attributes_for(:student, uid: current_student.uid, token: current_student.token)
+        patch :update, student: attributes_for(:student)
         expect(response).to redirect_to root_url
       end
     end
     context "with invalid attributes" do
       it "does not save the attributes to student" do
         expect do
-          patch :update, student: attributes_for(:student, first_name: nil, uid: current_student.uid, token: current_student.token)
+          patch :update, student: attributes_for(:student, first_name: nil)
           current_student.reload
         end.not_to change(current_student, :first_name)
       end
       it "re-renders :edit" do
-        patch :update, student: attributes_for(:student, first_name: nil, uid: current_student.uid, token: current_student.token)
+        patch :update, student: attributes_for(:student, first_name: nil)
         expect(response).to render_template :edit
       end
     end
