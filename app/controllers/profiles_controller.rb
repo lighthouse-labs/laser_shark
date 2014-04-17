58f83cf9bb3c5d4a9c2d7021c6857e4a10e72a15
matrinox <geofflee21@me.com>
2014-04-17 00:41:23 -0700

    Remove duplicate code inherited from ApplicationController

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,5 @@
 class ProfilesController < ApplicationController
 
-  protect_from_forgery with: :exception
-  before_action :authenticate
-
   def edit
     @form = RegistrationForm.new(current_student)
   end
