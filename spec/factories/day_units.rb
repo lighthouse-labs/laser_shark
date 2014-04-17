1b29ebe4b18306047c9c8e769f2e25958da6735a
Khurram Virani <kvirani@gmail.com>
2014-04-16 18:07:20 -0700

    better data for tests

--- spec/factories/day_units.rb
+++ spec/factories/day_units.rb
@@ -2,9 +2,9 @@
 
 FactoryGirl.define do
   factory :day_unit do
-    name "MyString"
-    day 1
-    start_time 1
-    duration 1
+    name { Faker::Commerce.product_name }
+    day { rand(1..50) }
+    start_time { [900, 1100, 1500, 1900, 2200].sample }
+    duration { rand(60..180) }
   end
 end
