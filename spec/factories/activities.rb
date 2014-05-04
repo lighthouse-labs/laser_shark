ad54b9df110062c1d80a76462fa703d5361fb4c6
Khurram Virani <kvirani@gmail.com>
2014-05-04 09:53:39 -0700

    fix test
    activity factory update

--- spec/factories/activities.rb
+++ spec/factories/activities.rb
@@ -3,7 +3,7 @@
 FactoryGirl.define do
   factory :activity do
     name { Faker::Commerce.product_name }
-    day { rand(1..50) }
+    day { "w#{rand(1..8)}d#{rand(1..5)}" }
     start_time { [900, 1100, 1500, 1900, 2200].sample }
     duration { rand(60..180) }
   end
