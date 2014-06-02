671a1d41de5ccce7031cf82859604dd3f120c029
Khurram Virani <kvirani@gmail.com>
2014-06-02 11:49:29 -0700

    only ask for public repo access

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: 'user, repo, gist'
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: 'user, public_repo, gist'
 end
