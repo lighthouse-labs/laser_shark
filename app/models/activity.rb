a8401b4ace8a3b3c6559e2bbd7086811ce643a3d
vagrant <suyonsohn@gmail.com>
2014-06-06 23:39:32 +0000

    made search use inject which is much cleaner and cleaned up search form in sidebar

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -38,12 +38,7 @@ class Activity < ActiveRecord::Base
 
   def self.search(query)
     words = query.to_s.strip.split
-    query_string = []
-    words.each do |w|
-      query_string << "activities.name ILIKE '%#{w}%'"
-    end
-    s = "SELECT activities.* FROM activities  WHERE " + query_string.join(' OR ')
-    Activity.find_by_sql s
+    words.inject(Activity.all) { |chain, word| chain.where("activities.name ILIKE ?", "%#{word}%") }
   end
 
   def next
