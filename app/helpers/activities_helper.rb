13f2e165fc036076b5d59b97c297b334f8b948f2
Gordon Wilkinson <gordonwilkinson@hotmail.com>
2014-06-06 19:24:38 +0000

    moved subclassed Redcarpet::Render::HTML to config/initialiers and added comments

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -12,8 +12,3 @@ module ActivitiesHelper
 
 end
 
-class TargetBlankRenderer < Redcarpet::Render::HTML
-  def initialize(extensions = {})
-    super extensions.merge(link_attributes: {target: "_blank"})
-  end
-end
