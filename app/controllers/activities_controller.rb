f53084233267b71befd9faa0e51f32282ea1ec2e
rene <r@c.com>
2014-06-06 16:34:45 +0000

    logic to search type working

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -4,16 +4,19 @@ class ActivitiesController < ApplicationController
 
   def show
     @activity = Activity.chronological.for_day(day).find(params[:id])
+
   end
 
   def search
 		@activities = search_activities(params[:query])
-
-	end
+    #@activities = Activity.chronological.for_day("w1d1")
+  end
+  #helper_method :search
 
 	def search_activities(q)
 
-		Activity.where('type LIKE ?', q).to_sql
+		Activity.where('type LIKE ?', q)
+
 
 	end
 
