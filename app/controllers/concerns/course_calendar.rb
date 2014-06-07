bae2261454854c7131f011410de138d830b03e38
Slav Kurilyak <kurilyak@gmail.com>
2014-06-07 21:28:44 +0000

    Implement fix for cohort keeps counting days and weeks

--- app/controllers/concerns/course_calendar.rb
+++ app/controllers/concerns/course_calendar.rb
@@ -22,22 +22,13 @@ module CourseCalendar
   end
 
   def date_accesibility_check
-    if !day_accessible?
-      today
-    elsif day_accessible? || !end_of_bootcamp?
-      @week_day
-    else
-      "w8d5"
-    end
+    return "w#{TOTAL_WEEKS}d#{TOTAL_DAYS}" if end_of_bootcamp?
+    return @week_day if day_accessible?
+    return today if !day_accessible?
   end
 
-  def day_accessible?
-    (week_number <= today_week_number) && (day_number <= today_day_number)
-  end
-
-
-  def today_is_after_end_of_bootcamp?
-    (today_week_number) > 8 || (today_week_number == 8 && today_day_number > 5)
+  def end_of_bootcamp?
+    (today_week_number) > TOTAL_WEEKS || (today_week_number == TOTAL_WEEKS && today_day_number > TOTAL_DAYS)
   end
 
   def today_week_number
@@ -45,12 +36,13 @@ module CourseCalendar
   end
 
   def today_day_number
-    today.split("d")[1].to_i
+    today_day_number = today.split("d")[1].to_i
+    return today_day_number if today_day_number != 0
+    return 6 if today_day_number == 0
   end
 
-
-  def end_of_bootcamp?
-    (week_number) > 8 || (week_number == 8 && day_number > 5)
+  def day_accessible?
+    (week_number <= today_week_number) && (day_number <= today_day_number)
   end
 
   def week_number
@@ -58,17 +50,13 @@ module CourseCalendar
   end
 
   def day_number
-    params[:number].split("d")[1].to_i
+    day_number = params[:number].split("d")[1].to_i
+    return day_number if day_number != 0
+    return 6 if day_number == 0
   end
-
 
   def today
     @today ||= CurriculumDay.new(Time.zone.now.to_date, cohort).to_s
-    # if !end_of_bootcamp?
-      # @today ||= CurriculumDay.new(Time.zone.now.to_date, cohort).to_s
-    # else
-    #   @today = "w8d5"
-    # end
   end
 
   def yesterday
@@ -76,7 +64,6 @@ module CourseCalendar
   end
 
   def allowed_day?
-    # raise day.inspect
     redirect_to(day_path('today'), alert: 'Access not allowed yet!') unless current_user.can_access_day?(day)
   end
 
