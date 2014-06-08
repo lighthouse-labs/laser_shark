89dd6a79c18ffc4b4f087e7c091c4b24d6a61361
Khurram Virani <kvirani@gmail.com>
2014-06-07 17:38:40 -0700

    fog added

--- app/uploaders/custom_avatar_uploader.rb
+++ app/uploaders/custom_avatar_uploader.rb
@@ -7,8 +7,11 @@ class CustomAvatarUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  if Rails.env.test? || Rails.env.development?
+    storage :file
+  else
+    storage :fog
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
