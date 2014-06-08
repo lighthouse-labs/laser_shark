cd495ceacd5ac9257c7bc40b1bd6d2ec92cf9311
billytianlan <billylan22@gmail.com>
2014-06-08 23:41:26 +0000

    final touches

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,10 +1,10 @@
 class StudentsController < ApplicationController
 
   def index
-    # @students = Student.where(cohort_id: params[:cohort_id]).all
 
     @cohort   = Cohort.find(params[:cohort_id])
     @students = @cohort.students
+    
   end
 
 end 
