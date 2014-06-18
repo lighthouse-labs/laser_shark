c1edd293e31d3e53e2e69eb14dea4b0340da19bb
Gabriel Taylor-Russ <gabriel.taylor.russ@gmail.com>
2014-06-18 14:44:22 -0700

    Changed hard coded reference to 'password' to reference ENV['ADMIN_PASSWORD']

--- spec/auth_helper.rb
+++ spec/auth_helper.rb
@@ -1,7 +1,7 @@
 module AuthHelper
 
   def set_valid_auth
-    request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials('admin', 'password')
+    request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials('admin', ENV['ADMIN_PASSWORD'])
   end
 
   def set_invalid_auth
--- spec/controllers/admin/cohorts_controller_spec.rb
+++ spec/controllers/admin/cohorts_controller_spec.rb
@@ -1,14 +1,15 @@
 require 'spec_helper'
 
 describe Admin::CohortsController do
-    before :each do
+  before :each do
     set_valid_auth
   end
 
   describe 'GET #index' do
     it 'assigns all cohorts to @cohorts' do
       cohorts = create_list(:cohort, 5)
-      get :index 
+      get :index
+      expect(assigns(:cohorts)).to_not be_nil
       expect(assigns(:cohorts)).to match_array(cohorts)
     end
 
