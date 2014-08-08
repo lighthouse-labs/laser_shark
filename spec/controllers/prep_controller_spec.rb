be8c0f1b84936b4fa924c2e1f7eba703247d272f
Eubene Sa <eubene.sa@gmail.com>
2014-08-08 14:25:47 -0700

    prep_controller_spec fixed, see comments in prep_controller_spec.rb

--- spec/controllers/prep_controller_spec.rb
+++ spec/controllers/prep_controller_spec.rb
@@ -2,6 +2,9 @@ require 'spec_helper'
 
 describe PrepController do
 
+  # User needs to be logged in for prep page to show
+  logged_in_user
+
   describe "GET 'show'" do
     it "returns http success" do
       get 'show'
