3956c89b028f4e674e2274d797f0255e6d923b82
Marlon Kenny <marlonkenny@gmail.com>
2014-10-04 22:03:34 +0000

    added change and remove actions to Admin::Students controller

--- app/controllers/admin/students_controller.rb
+++ app/controllers/admin/students_controller.rb
@@ -4,4 +4,16 @@ class Admin::StudentsController < Admin::BaseController
     @students = Student.all
   end
 
+  def change_cohort
+    @student = Student.find(params[:student_id])
+    @student.cohort = Cohort.find(params[:cohort_id])
+    @student.save
+    redirect_to :index, alert: "#{params.inspect}"
+  end
+
+  def remove_from_cohort
+    @student = Student.find(params[:student_id])
+    @cohort  = Cohort.find(params[:cohort_id])
+    @cohort.students.delete(@student)
+  end
 end
