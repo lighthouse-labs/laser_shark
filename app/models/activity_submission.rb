134438ec5153e4700e3165e08dce35517d2abc7d
Khurram Virani <kvirani@gmail.com>
2014-12-29 14:38:00 -0800

    allow everything to be checked as completed
    just dont require github_url for those marked as `!allow_submissions?`

--- app/models/activity_submission.rb
+++ app/models/activity_submission.rb
@@ -9,6 +9,15 @@ class ActivitySubmission < ActiveRecord::Base
   validates :user_id, uniqueness: { scope: :activity_id,
     message: "only one submission per activity" }
 
-  validates :github_url, presence: :true, format: { with: URI::regexp(%w(http https)), message: "must be a valid format" }
+  validates :github_url, 
+    presence: :true, 
+    format: { with: URI::regexp(%w(http https)), message: "must be a valid format" },
+    if: :github_url_required?
+
+  private
+
+  def github_url_required?
+    activity && activity.allow_submissions?
+  end
 
 end
