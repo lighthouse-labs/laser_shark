7b93ef47ce7e5b8511ac43069e0a6cf76384ce1e
Maggie Moss <maggiebmoss@gmail.com>
2015-02-08 00:52:50 +0000

    admin can edit student types

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ LaserShark::Application.routes.draw do
   # ADMIN
   namespace :admin do
     root to: 'dashboard#show'
-    resources :students, only: [:index]
+    resources :students, only: [:index, :create, :edit, :update, :destroy, :show]
     resources :cohorts, only: [:index]
   end
 
