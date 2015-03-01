22d4f08048503e128b345cc15e2436a6f4cabdc8
David VanDusen <github@davidvandusen.com>
2015-03-01 11:33:21 -0800

    Update schema.rb after migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150128034138) do
+ActiveRecord::Schema.define(version: 20150301162504) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20150128034138) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "assistee_id"
+    t.integer  "rating"
   end
 
   create_table "cohorts", force: true do |t|
