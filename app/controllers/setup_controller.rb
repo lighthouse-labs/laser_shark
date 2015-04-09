6d1c9fa7bc36f68a7c0584377b0ca945f6a5f1e2
Khurram Virani <kvirani@gmail.com>
2015-04-08 20:45:28 -0700

    display duration in setup page

--- app/controllers/setup_controller.rb
+++ app/controllers/setup_controller.rb
@@ -5,6 +5,7 @@ class SetupController < ApplicationController
   skip_before_action :allowed_day?
 
   def show
+    @setup = true
     @activities = Activity.chronological.for_day("setup")
   end
 
--- app/helpers/days_helper.rb
+++ app/helpers/days_helper.rb
@@ -1,5 +1,9 @@
 module DaysHelper
 
+  def setup?
+    @setup
+  end
+
   def day_status_css(d)
     classes = []
     classes.push('disabled') unless current_user.can_access_day?(d)
