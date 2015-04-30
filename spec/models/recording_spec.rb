21e6081c5a026d1e66dd20bc41cba356157574b1
David VanDusen <github@davidvandusen.com>
2015-04-30 08:25:44 -0700

    Fix indentation in recording_spec.rb.

--- spec/models/recording_spec.rb
+++ spec/models/recording_spec.rb
@@ -1,15 +1,15 @@
 require 'spec_helper'
 
 describe Recording do
-	it "has a valid factory" do
-		expect(build(:recording)).to be_valid
-	end
+  it "has a valid factory" do
+    expect(build(:recording)).to be_valid
+  end
 
   it "is invalid without a program" do
-  	expect(build(:recording, program: nil)).to have(1).errors_on(:program)
+    expect(build(:recording, program: nil)).to have(1).errors_on(:program)
   end
 
   it "is invalid with a program that does not specify a recordings_bucket" do
-  	expect(build(:recording, program: create(:program, recordings_bucket: nil))).to have(1).errors_on(:program)
+    expect(build(:recording, program: create(:program, recordings_bucket: nil))).to have(1).errors_on(:program)
   end
 end
