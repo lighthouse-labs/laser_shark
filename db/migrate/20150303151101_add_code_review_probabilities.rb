337c2cd1694f3ff2ad8a72c61869c27341b3091b
David VanDusen <github@davidvandusen.com>
2015-05-10 09:19:27 -0400

    Remove obsolete migration.

--- db/migrate/20150303151101_add_code_review_probabilities.rb
+++
@@ -1,6 +0,0 @@
-class AddCodeReviewProbabilities < ActiveRecord::Migration
-  def change
-    add_column :activities, :code_review_percent, :integer, default: 60
-    add_column :users, :code_review_percent, :integer, default: 80
-  end
-end
