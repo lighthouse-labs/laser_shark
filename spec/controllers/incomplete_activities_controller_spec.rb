fe96814ffc5112e5bfc31d577f0f4c8dade2b5d4
LaithAzer <laith.azer@gmail.com>
2015-08-03 21:46:50 +0000

    added a test

--- spec/controllers/incomplete_activities_controller_spec.rb
+++ spec/controllers/incomplete_activities_controller_spec.rb
@@ -2,4 +2,15 @@ require 'spec_helper'
 
 describe IncompleteActivitiesController do
 
+  logged_in_user
+
+  describe "GET #incomplete" do
+    it "gets all incompleted assignments up until today" do
+      current_user.cohort = Cohort.new name: "test_cohort", start_date: Date.today, code: 1
+      current_user.save
+      get :index
+      expect(response).to render_template :index
+    end
+  end
+
 end
