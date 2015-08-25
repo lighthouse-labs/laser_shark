8f942a0635b5079ebdea7f885f113663fc0233d3
theabuitendyk <theabuitendyk@users.noreply.github.com>
2015-08-25 15:12:59 -0700

    create/index

--- app/controllers/absents_controller.rb
+++ app/controllers/absents_controller.rb
@@ -1,7 +1,46 @@
 class AbsentsController < ApplicationController
+  
+  include CourseCalendar # concern
+
+  before_action :require_teacher
+
   def create
+    # TODO - Day changes at 6pm? so 'today' on a fri night converts to weekend ex w-1e
+    @formatted_date = CurriculumDay.new(params[:day_number], cohort).to_s
+    @students = cohort.students
+    @students.each do |student|
+      if params[:student_ids] && params[:student_ids].include?("#{student.id}")
+        @absent = Absent.find_or_create_by(student_id: student.id, date: @formatted_date)
+      else 
+        @absent = Absent.destroy_all(student_id: student.id, date: @formatted_date)
+      end
+    end
+
+    redirect_to day_path(params[:day_number]), notice: 'Attendance taken!'
   end
 
   def index
+    @absent = Absent.new
+    @students = cohort.students
+
+    @status = @students.collect{|student| student.absent?(params[:day_number])}
+
+    @attendance = true
   end
+  
+  private
+  
+  def absent_params
+    params.require(:absent).permit(
+      :student_ids
+    )
+  end
+
+  protected
+
+  # TODO - access from somewhere else?
+  def require_teacher
+    redirect_to :root, alert: 'Only teachers can do this!' unless teacher?
+  end
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,6 +2,7 @@ class Student < User
 
   belongs_to :cohort
   has_many :day_feedbacks, foreign_key: :user_id
+  has_many :absents, foreign_key: :user_id
   
   scope :in_active_cohort, -> { joins(:cohort).merge(Cohort.is_active) }
 
@@ -19,6 +20,10 @@ class Student < User
 
   def alumni?
     !prepping? && cohort.finished?
+  end
+
+  def absent?(day)
+    !Absent.where(student_id: self.id, date: day).empty?
   end
 
 end
