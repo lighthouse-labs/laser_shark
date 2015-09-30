206f7ae931f3dd8834d303aebcaaca5b30fcfb73
Laith Azer <laith.azer@gmail.com>
2015-09-30 17:29:11 -0400

    card 44 - admin can browse feedback

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -5,7 +5,7 @@ class Feedback < ActiveRecord::Base
   belongs_to :teacher
 
   scope :completed, -> { where("technical_rating IS NOT NULL AND style_rating IS NOT NULL") }
-  scope :pending, -> { where("technical_rating IS NULL OR style_rating IS NULL") }
+  scope :pending, -> { where("technical_rating IS NULL AND style_rating IS NULL") }
   scope :reverse_chronological_order, -> { order("updated_at DESC") }
   scope :filter_by_student, -> (student_id) { where("student_id = ?", student_id) }
   scope :filter_by_teacher, -> (teacher_id) { where("teacher_id = ?", teacher_id) }
@@ -28,15 +28,19 @@ class Feedback < ActiveRecord::Base
     options.inject(all) do |result, (k, v)|
       attribute = k.gsub("_id", "")
       if attribute == 'completed?'
-        if v == 'true'
-          result = result.completed
-        else
-          result = result.pending
-        end
+        result = self.filter_by_completed(v, result)
       else
         result = result.send("filter_by_#{attribute}", v)
       end    
     end
+  end
+
+  def self.filter_by_completed(value, result)
+    if value == 'true'
+      result = result.completed
+    else
+      result = result.pending
+    end  
   end
 
 end
