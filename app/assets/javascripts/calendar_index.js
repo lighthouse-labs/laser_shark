a1ed270398974d42c54e1baf35cbf765bfe64a01
Laith Azer <laith.azer@gmail.com>
2015-10-01 17:31:49 -0400

    card 9 - calendar of events filters by day

--- app/assets/javascripts/calendar_index.js
+++ app/assets/javascripts/calendar_index.js
@@ -4,23 +4,24 @@
     var $div = $(this);
 
     var defaults = $.extend({
-        calendarId: 'en.usa#holiday@group.v.calendar.google.com',
-        apiKey: 'Public_API_Key',
-        dateFormat: 'LongDate',
+        calendarId: 'en.canadian#holiday@group.v.calendar.google.com',
+        apiKey: 'AIzaSyA0e9Ts3CSV9CQ-dOV-Pxwc2d5EszJBFLI',
+        startDate: new Date().toISOString(),
+        endDate: new Date().toISOString(),
         errorMsg: 'No events in calendar',
         maxEvents: 50,
-        futureEventsOnly: true,
         sortDescending: true
       },
       options);
 
     var feedUrl = 'https://www.googleapis.com/calendar/v3/calendars/' +
       encodeURIComponent(defaults.calendarId.trim()) +'/events?key=' + defaults.apiKey +
-      '&orderBy=startTime&singleEvents=true';
-      if(defaults.futureEventsOnly) {
-        feedUrl+='&timeMin='+ new Date().toISOString();
-      }
+      '&orderBy=startTime&singleEvents=true' + '&timeMin=' + defaults.startDate + '&timeMax=' + defaults.endDate;
 
+    var eventsHeader = $div.prev();
+    eventsHeader.hide();
+    eventsHeader.prev().hide();
+      
     $.ajax({
       url: feedUrl,
       dataType: 'json',
@@ -28,20 +29,27 @@
         if(defaults.sortDescending){
           data.items = data.items.reverse();
         }
+        // If no events on that day, don't show events section
+        if (data.items.length != 0) {
+          eventsHeader.show();
+          eventsHeader.prev().show();
+          $.each(data.items, function(e, item) {
+            var summary = item.summary || '';
+            var link = item.htmlLink;
+            var location = item.location || '';
+            var details = '<div class="icon icon-type"><i class="fa fa-calendar"></i></div>'
+            details += '<a href=' + link + '><div class="name">' + summary + '<div class="location">' + location + '</div>' + '</div></a>';
+            if (item.start.dateTime) {
+              var startTime = item.start.dateTime.substring(11,16);
+              var endTime = item.end.dateTime.substring(11,16);
+              details += '<div class="time">' + startTime + '<br> to ' + endTime + '<br>' + '</div>';
+            }
+            calendarEvent = '<div class="calendar activity">' + details + '</div>';
+            $($div).append(calendarEvent);
+          });
+        }
         data.items = data.items.slice(0, defaults.maxEvents);
 
-        $.each(data.items, function(e, item) {
-          var startTime = item.start.dateTime.substring(11,16);
-          var endTime = item.end.dateTime.substring(11,16);
-          var summary = item.summary || '';
-          var link = item.htmlLink;
-          var location = item.location || '';
-          var details = '<div class="icon icon-type"><i class="fa fa-calendar"></i></div>'
-          details += '<a href=' + link + '><div class="name">' + summary + '<div class="location">' + location + '</div>' + '</div></a>';
-          details += '<div class="time">' + startTime + '<br> to ' + endTime + '<br>' + '</div>';
-          s = '<div class="calendar activity">' + details + '</div>';
-          $($div).append(s);
-        });
       },
       error: function(xhr, status) {
         $($div).append('<p>' + status +' : '+ defaults.errorMsg +'</p>');
--- app/assets/javascripts/calendar_of_events.js.coffee
+++ app/assets/javascripts/calendar_of_events.js.coffee
@@ -1,34 +1,53 @@
 $ ->
 
-  list = $('#eventlist')  
-  location = list.data 'location'
-  if location is 'Toronto'
-    calendarLocation = 'functionalimperative.com_du453ucqlvlir8rtf1sdjdd1ak@group.calendar.google.com'
-  else
-    # Need to get Vancouver calendar address
-    calendarLocation = 'en.canadian#holiday@group.v.calendar.google.com'
+  getEvents = (calendar, startDate, endDate) ->
 
-  $('#eventlist').gCalReader
+    $('#eventlist').gCalReader
 
-    # Public Google Calendar
-    # calendarId:'functionalimperative.com_du453ucqlvlir8rtf1sdjdd1ak@group.calendar.google.com'
-    calendarId: calendarLocation
+      calendarId: calendar
 
-    # Google API KEY
-    # I think we need to put one for compass
-    apiKey:'AIzaSyA0e9Ts3CSV9CQ-dOV-Pxwc2d5EszJBFLI'
+      startDate: startDate
+      endDate: endDate
 
-    # <a href="http://www.jqueryscript.net/time-clock/">date</a> format
-    dateFormat: 'LongDate'
+  getUserDetails = ->
+       
+    list = $('#eventlist')  
+    location = list.data 'location'
+    program = list.data 'program'
+    day = list.data 'day'
+    cohortStartDate = list.data 'start-date'
+    startDate = calculateDateOfCurriculumDay(day, cohortStartDate, 'start')
+    endDate = calculateDateOfCurriculumDay(day, cohortStartDate, 'end')
+    # web = program.indexOf 'Web'
+    # iOS = program.indexOf 'iOS'
+    if location is 'Toronto'
+      calendarLocation = 'functionalimperative.com_du453ucqlvlir8rtf1sdjdd1ak@group.calendar.google.com'
+    else
+      # Need to get Vancouver calendar address
+      calendarLocation = 'en.canadian#holiday@group.v.calendar.google.com'      
+    getEvents(calendarLocation, startDate, endDate)
 
-    # error message
-    errorMsg: 'No events in calendar'
+  calculateDateOfCurriculumDay = (day, startDate, type) ->
+    # Get which week/day is currently being viewed
+    # Subtract one because if they are week 1, should not add an extra week. Similaraly, if day 2 should only add 1 day and not 2.
+    weeks = parseInt(day[1]) - 1
+    # If day is a weekend the number of days to add is 5
+    if day[3] is 'e'
+      days = 5
+    else
+      days = parseInt day[3] - 1
 
-    # maximum events
-    maxEvents: 50
+    # Get the date that curriculum day translates to, e.g. w2d1 would be 1 week after the start date
+    dateYear = startDate.substring(0,4)
+    dateMonth = startDate.substring(5,7)
+    dateDay = startDate.substring(8,10)
+    startDate = dateMonth + '-' + dateDay + '-' + dateYear
+    currentDate = new Date startDate
+    currentDate.setDate(currentDate.getDate() + (weeks * 7 + days))
+    if type is 'start'
+      currentDate.setHours(1)
+    else
+      currentDate.setHours(23)
+    return currentDate.toISOString()
 
-    # future-events filter
-    futureEventsOnly: true
-
-    # descending sort order
-    sortDescending: true
+  getUserDetails()  
