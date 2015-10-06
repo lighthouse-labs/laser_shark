f5655392df2b35e205d534367e1470284aaed049
Khurram Virani <kvirani@gmail.com>
2015-10-06 15:24:49 -0700

    set up staging env to be able to use something like mailtrap

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -72,12 +72,13 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify  
 
-    config.action_mailer.smtp_settings = {
-      :address   => "smtp.mandrillapp.com",
-      :port      => 587,
-      :user_name => ENV["MANDRILL_USERNAME"],
-      :password  => ENV["MANDRILL_APIKEY"]
-    }
+  config.action_mailer.smtp_settings = {
+    :address   => ENV["SMTP_SERVER"]   || "smtp.mandrillapp.com",
+    :port      => ENV["SMTP_PORT"]     || 587,
+    :user_name => ENV["SMTP_USERNAME"] || ENV["MANDRILL_USERNAME"],
+    :password  => ENV["SMTP_APIKEY"]   || ENV["MANDRILL_APIKEY"]
+  }
+  
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => ENV['HOST'] }
   config.action_mailer.delivery_method = :smtp
