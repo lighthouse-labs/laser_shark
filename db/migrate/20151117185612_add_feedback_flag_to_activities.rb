a8dd095c5b390810c172df9b24b308a3caa64a4a
Laith Azer <laith.azer@gmail.com>
2015-11-18 11:27:54 -0500

    card 81 - change migration to use lecture model

--- db/migrate/20151117185612_add_feedback_flag_to_activities.rb
+++ db/migrate/20151117185612_add_feedback_flag_to_activities.rb
@@ -1,7 +1,7 @@
 class AddFeedbackFlagToActivities < ActiveRecord::Migration
   def up
     add_column :activities, :allow_feedback, :boolean, :default => true
-    Activity.where(type: 'Lecture').update_all(allow_feedback: false)
+    Lecture.update_all(allow_feedback: false)
   end
 
   def down
