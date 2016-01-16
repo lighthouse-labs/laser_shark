73e75338b4af230c467f7d78491b8a804559648a
Graham Grochowski <ggrochow@gmail.com>
2016-01-15 18:02:31 -0800

    admin student edit page

--- app/controllers/admin/students_controller.rb
+++ app/controllers/admin/students_controller.rb
@@ -1,5 +1,5 @@
 class Admin::StudentsController < Admin::BaseController
-  before_action :load_student, only: [:reactivate, :deactivate, :update]
+  before_action :load_student, only: [:reactivate, :deactivate, :update, :edit]
 
   def index
     if params[:cohort_id]
@@ -8,6 +8,10 @@ class Admin::StudentsController < Admin::BaseController
     else
       @students = Student.all
     end
+    @cohorts = Cohort.is_active
+  end
+
+  def edit
     @cohorts = Cohort.is_active
   end
 
@@ -22,12 +26,27 @@ class Admin::StudentsController < Admin::BaseController
   end
 
   def update
-    @cohort = Cohort.find(params[:cohort_id])
-    @student.update(cohort: @cohort)
-    render nothing: true if @student.save
+    if @student.update(student_params)
+      redirect_to [:edit, :admin, @student], notice: 'Updated!'
+    else
+      render :edit
+    end    
   end
 
   private
+
+  def student_params
+    params.require(:student).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :github_username,
+      :type,
+      :unlocked_until_day,
+      :cohort_id
+    )
+
+  end
 
   def load_student
     @student = Student.find(params[:id])
