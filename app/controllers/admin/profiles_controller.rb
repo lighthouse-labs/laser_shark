8825b5bcf37bc8407f86dd95761922bb3b916111
Graham Grochowski <ggrochow@gmail.com>
2016-01-22 15:44:33 -0800

    controller logic for update route

--- app/controllers/admin/profiles_controller.rb
+++ app/controllers/admin/profiles_controller.rb
@@ -6,9 +6,25 @@ class Admin::ProfilesController < Admin::BaseController
   end
 
   def update
+    if @user.update profile_params
+      redirect_to root_url
+    else
+      render :edit
+    end
   end
 
   private
+
+  def profile_params
+    params.require(:user).permit(
+        :first_name,
+        :last_name,
+        :phone_number,
+        :email,
+        :location_id,
+        :receive_feedback_emails
+      )
+  end
 
   def load_user
     @user = current_user
