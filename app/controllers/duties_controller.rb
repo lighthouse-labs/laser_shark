04072613dd2ef778bc45bffdbcd442550be75a8f
Ajwah <johnson786.kevin+git@gmail.com>
2016-02-10 20:00:21 -0500

    card 144 Add format_channel_name to `duties_controller`.

--- app/controllers/duties_controller.rb
+++ app/controllers/duties_controller.rb
@@ -1,26 +1,44 @@
 class DutiesController < ApplicationController
 
   def on_duty
+
     if current_user.is_a?(Teacher)
+
       current_user.update(on_duty: true)
 
-      Pusher.trigger 'TeacherChannel', 'received', {
-        type: "TeacherOnDuty",
-        object: UserSerializer.new(current_user).as_json
-      }
+      Pusher.trigger format_channel_name('TeacherChannel'),
+                     'received', {
+                        type: "TeacherOnDuty",
+                        object: UserSerializer.new(current_user).as_json
+                     }
+
+      head :ok, content_type: "text/html"
+
+    else
+
+      permission_denied
+
     end
-    head :ok, content_type: "text/html"
   end
 
   def off_duty
+
     if current_user.is_a?(Teacher)
+
       current_user.update(on_duty: false)
 
-      Pusher.trigger 'TeacherChannel', 'received', {
-        type: "TeacherOffDuty",
-        object: UserSerializer.new(current_user).as_json
-      }
+      Pusher.trigger format_channel_name('TeacherChannel'),
+                     'received', {
+                        type: "TeacherOffDuty",
+                        object: UserSerializer.new(current_user).as_json
+                     }
+
+      head :ok, content_type: "text/html"
+
+    else
+
+      permission_denied
+
     end
-    head :ok, content_type: "text/html"
   end
 end
