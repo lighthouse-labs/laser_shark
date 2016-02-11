38ead867a1b686f16e5ff288f879052f262f915e
Ajwah <johnson786.kevin+git@gmail.com>
2016-02-10 19:54:08 -0500

    card 144 Add format_channel_name to assistances_requests_controller.

--- app/controllers/assistance_requests_controller.rb
+++ app/controllers/assistance_requests_controller.rb
@@ -1,15 +1,26 @@
 class AssistanceRequestsController < ApplicationController
 
   #before_action :selected_cohort_locations, only: [:index, :queue]
-  before_filter :teacher_required, only: [:index, :destroy, :start_assistance, :end_assistance, :queue]
+  before_filter :teacher_required, only: [ :index, :destroy, :start_assistance,
+                                           :end_assistance, :queue ]
 
   def subscribed
-    Pusher.trigger("UserChannel#{current_user.id}", 'received', {type: "UserConnected", object: UserSerializer.new(current_user).as_json})
+    Pusher.trigger format_channel_name("UserChannel", current_user.id),
+                   'received', {
+                      type: "UserConnected",
+                      object: UserSerializer.new(current_user).as_json
+                    }
+
     head :ok, content_type: "text/html"
   end
 
   def index
-    @all_locations = Location.where("id IN (?)", Cohort.all.map(&:location_id).uniq).map{|l| LocationSerializer.new(l, root: false).as_json}
+    no_root =
+    location_ids = Cohort.all.map(&:location_id).uniq
+    @all_locations = Location.where("id IN (?)", location_ids)
+                             .map do |l|
+                                LocationSerializer.new(l, root: false).as_json
+                              end
 
     render component: "RequestQueue",
       props: {
@@ -22,52 +33,104 @@ class AssistanceRequestsController < ApplicationController
   def create
     ar = AssistanceRequest.new(requestor: current_user,
                                reason: assistance_request_params[:reason])
-    ar.save
 
-    Pusher.trigger("assistance-#{current_user.cohort.location.name}",
-                   'received', {
-                    type: "AssistanceRequest",
-                    object: AssistanceRequestSerializer.new(ar, root: false).as_json
-                  })
+    if ar.save
 
-    Pusher.trigger("UserChannel#{current_user.id}",
-                   'AssistanceRequested',
-                   object: current_user.position_in_queue  )
-    head :ok, content_type: "text/html"
+      location_name = current_user.cohort.location.name
+      serialized_ar = AssistanceRequestSerializer.new(ar, root: false).as_json
+
+      Pusher.trigger format_channel_name("assistance", location_name),
+                     'received', {
+                       type: "AssistanceRequest",
+                       object: serialized_ar
+                     }
+
+      Pusher.trigger format_channel_name("UserChannel", current_user.id),
+                     'received', {
+                        type: 'AssistanceRequested',
+                        object: current_user.position_in_queue
+                      }
+
+      head :ok, content_type: "text/html"
+
+    else
+
+      permission_denied
+
+    end
   end
 
   def cancel
-    ar = current_user.assistance_requests.where(type: nil).open_or_in_progress_requests.newest_requests_first.first
+    ar = current_user.assistance_requests
+                     .where(type: nil)
+                     .open_or_in_progress_requests
+                     .newest_requests_first
+                     .first
+
     if ar && ar.cancel
 
-      Pusher.trigger("assistance-#{current_user.cohort.location.name}",
-                     'received', {
-                      type: "CancelAssistanceRequest",
-                      object: AssistanceRequestSerializer.new(ar, root: false).as_json
-                    })
+      location_name = current_user.cohort.location.name
+      serialized_ar = AssistanceRequestSerializer.new(ar, root: false).as_json
 
-      Pusher.trigger("UserChannel#{current_user.id}", 'AssistanceEnded', {})
+      Pusher.trigger format_channel_name("assistance", location_name),
+                     'received', {
+                        type: "CancelAssistanceRequest",
+                        object: serialized_ar
+                      }
+
+      Pusher.trigger format_channel_name("UserChannel", current_user.id),
+                     'received', {
+                        type: 'AssistanceEnded'
+                      }
 
       teacher_available(ar.assistance.assistor) if ar.assistance
       update_students_in_queue(ar.requestor.cohort.location.name)
+
+      head :ok, content_type: "text/html"
+
+    else
+
+      permission_denied
+
     end
-    head :ok, content_type: "text/html"
   end
 
   def queue
-    my_active_assistances = Assistance.assisted_by(current_user).currently_active
-    requests = AssistanceRequest.where(type: nil).open_requests.oldest_requests_first.requestor_cohort_in_locations([params[:location]])
-    code_reviews = CodeReviewRequest.open_requests.oldest_requests_first.requestor_cohort_in_locations([params[:location]])
-    all_students = Student.in_active_cohort.active.order_by_last_assisted_at.cohort_in_locations([params[:location]])
+    location = params[:location]
 
-    render json: RequestQueueSerializer.new(assistances: my_active_assistances, requests: requests, code_reviews: code_reviews, students: all_students).as_json
+    my_active_assistances = Assistance.assisted_by(current_user)
+                                      .currently_active
+
+    requests = AssistanceRequest.where(type: nil)
+                                .open_requests
+                                .oldest_requests_first
+                                .requestor_cohort_in_locations([location])
+
+    code_reviews = CodeReviewRequest.open_requests
+                                    .oldest_requests_first
+                                    .requestor_cohort_in_locations([location])
+
+    all_students = Student.in_active_cohort
+                          .active
+                          .order_by_last_assisted_at
+                          .cohort_in_locations([location])
+
+    render json: RequestQueueSerializer.new (
+                    assistances: my_active_assistances,
+                    requests: requests,
+                    code_reviews: code_reviews,
+                    students: all_students
+                 ).as_json
   end
 
   def status
     respond_to do |format|
       format.json {
         # Fetch most recent student initiated request
-        ar = current_user.assistance_requests.where(type: nil).newest_requests_first.first
+        ar = current_user.assistance_requests
+                         .where(type: nil)
+                         .newest_requests_first
+                         .first
         res = {}
         if ar.try(:open?)
           res[:state] = :waiting
@@ -107,7 +170,10 @@ class AssistanceRequestsController < ApplicationController
     locations_cookie = cookies[:cohort_locations]
     # The length thing is a hacky/lazy of getting around a JSON parse exception - KV
     #   http://stackoverflow.com/questions/8390256/a-json-text-must-at-least-contain-two-octets
-    @selected_locations = locations_cookie && locations_cookie.to_s.length >= 2 ? JSON.parse(locations_cookie) : []
+
+    is_parsable? = locations_cookie && locations_cookie.to_s.length >= 2
+
+    @selected_locations = is_parsable? ? JSON.parse(locations_cookie) : []
   end
 
   def teacher_required
