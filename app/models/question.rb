0de005d5e3ff88499b9b7b3d3a33de79a3cdd93f
Paul Iogna <paul.iogna@gmail.com>
2016-03-30 11:14:51 -0400

    add validations and associations for question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,13 @@
 class Question < ActiveRecord::Base
+  belongs_to :activity
+
+  has_many :options, dependent: :destroy
+
+  accepts_nested_attributes_for :options, allow_destroy: true
+
+  has_and_belongs_to_many :quizzes
+
+  validates :question, presence: true
+
+  validates :activity, presence: true
 end
