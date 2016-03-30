64f0a2689c0ec2264d5a09de75bce5eea8ae6e23
Paul Iogna <paul.iogna@gmail.com>
2016-03-30 11:00:48 -0400

    add validations and associations for quiz_submission

--- app/models/quiz_submission.rb
+++ app/models/quiz_submission.rb
@@ -1,2 +1,20 @@
 class QuizSubmission < ActiveRecord::Base
+
+  belongs_to :student
+
+  has_many :answers, dependent: :destroy
+
+  belongs_to :quiz
+
+  accepts_nested_attributes_for :answers
+
+  before_validation on: :create do
+    unless uuid
+      self.uuid = SecureRandom.uuid
+    end
+  end
+
+  def to_param
+    uuid
+  end
 end
