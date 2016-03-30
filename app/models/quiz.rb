09bf56ac595ff75419b37031a435705d92b05154
Paul Iogna <paul.iogna@gmail.com>
2016-03-30 10:08:41 -0400

    add validations and associations to quiz

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,2 +1,25 @@
 class Quiz < ActiveRecord::Base
+  belongs_to :activity
+
+  has_many :submissions, dependent: :nullify
+
+  has_and_belongs_to_many :questions
+
+  validates :cohort, presence: true
+
+  validates :day, presence: true
+
+  validate do
+    errors.add(:questions, "insufficient for a quiz; #{QUESTIONS_PER_QUIZ} needed") if questions.length < QUESTIONS_PER_QUIZ
+  end
+
+  before_validation on: :create do
+    unless uuid
+      self.uuid = SecureRandom.uuid
+    end
+  end
+
+  def to_param
+    uuid
+  end
 end
