62e4c7a3d589d48de1adf0c416bc2f3dc6be5095
Paul Iogna <paul.iogna@gmail.com>
2016-03-31 17:55:35 -0400

    fix submission save

--- app/controllers/quiz_submissions_controller.rb
+++ app/controllers/quiz_submissions_controller.rb
@@ -5,11 +5,20 @@ class QuizSubmissionsController < ApplicationController
   end
 
   def create
-    quiz_submission = QuizSubmission.new(params[:quiz_submission])
-    quiz_submission.save
+    @quiz_submission = QuizSubmission.new(submission_params)
+    @quiz_submission.user = current_user
+    if @quiz_submission.save
+      render :show
+    else
+      render :new
+    end
   end
 
   def show
   end
 
+  private
+  def submission_params
+    params.require(:quiz_submission).permit(:quiz_id, answers_attributes: [:option_id])
+  end
 end
