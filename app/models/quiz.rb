e7f2c8baab88af1028d4f98075680da9747e24e1
Paul Iogna <paul.iogna@gmail.com>
2016-04-04 14:07:52 -0400

    add 5 question minimum to quiz

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,4 +1,7 @@
 class Quiz < ActiveRecord::Base
+
+  QUESTIONS_PER_QUIZ = 5
+
   has_many :quiz_activities
 
   has_many :quiz_submissions, dependent: :nullify
@@ -9,9 +12,9 @@ class Quiz < ActiveRecord::Base
 
   validates :day, presence: true
 
-  # validate do
-  #   errors.add(:questions, "insufficient for a quiz; #{QUESTIONS_PER_QUIZ} needed") if questions.length < QUESTIONS_PER_QUIZ
-  # end
+  validate do
+    errors.add(:questions, "insufficient for a quiz; #{QUESTIONS_PER_QUIZ} needed") if questions.length < QUESTIONS_PER_QUIZ
+  end
 
   # before_validation on: :create do
   #   unless uuid
