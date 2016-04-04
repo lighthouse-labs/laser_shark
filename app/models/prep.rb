431ea588ecdd8ba62847f842e046cad144f72cc2
Jonathan Salis <jon@functionalimperative.com>
2016-04-04 10:26:24 -0400

    card 154 - Changing prep rating calc

--- app/models/prep.rb
+++ app/models/prep.rb
@@ -2,7 +2,7 @@ class Prep < Section
 
   def self.evaluate_rating(user_activity_outcome)
     submission_num = user_activity_outcome.user.activity_submissions.where(activity: user_activity_outcome.activity_outcome.activity).count
-    (1 / submission_num * 3).to_i
+    (1.0 / submission_num * 3).to_i
   end
 
 end
