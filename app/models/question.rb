d1e97b4ba32e63ccacffcb2bbb339cf7ca922c00
Paul Iogna <paul.iogna@gmail.com>
2016-04-04 09:19:15 -0400

    add validation for questions. must have only 1 correct option.

--- app/models/question.rb
+++ app/models/question.rb
@@ -9,4 +9,15 @@ class Question < ActiveRecord::Base
   validates :question, presence: true
 
   # validates :activity, presence: true
+
+  validate :only_one_correct_option
+
+  def only_one_correct_option
+    count = 0
+    options.each do |option|
+      count += 1 if option.correct
+    end
+    errors.add(:options, "can't have more than one correct option") if count > 1
+    errors.add(:options, "must have at least one correct option") if count < 1
+  end
 end
