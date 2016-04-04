53143edc3076b225b5a5dd2b7dd351c07025f949
Jonathan Salis <jon@functionalimperative.com>
2016-04-04 12:29:27 -0400

    Adding horrible message about the migration

--- db/migrate/20160404145127_change_user_activity_outcome_to_polymorphic.rb
+++ db/migrate/20160404145127_change_user_activity_outcome_to_polymorphic.rb
@@ -1,6 +1,8 @@
 class ChangeUserActivityOutcomeToPolymorphic < ActiveRecord::Migration
   def change
     file_path = File.join(Rails.root, "app", "models", "user_activity_outcome.rb")
+
+    # => This will never run for anyone, but it worked for me because I had the model before commiting the delete of it
     if File.exists?(file_path)
       UserActivityOutcome.destroy_all
       drop_table :user_activity_outcomes
