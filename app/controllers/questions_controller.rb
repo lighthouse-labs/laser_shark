90b8391bd1447578d41b42da132390a3db0da2b4
Paul Iogna <paul.iogna@gmail.com>
2016-04-06 09:09:07 -0400

    hide questions controller logic

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,42 +1,39 @@
 class QuestionsController < ApplicationController
-  def index
-    @questions = Question.all
-  end
+  # def index
+  #   @questions = Question.all
+  # end
 
-  def show
-    @question = Question.find(params[:id])
-    respond_to do |format|
-      format.html
-      format.json { render json: @question.to_json(include: :options )}
-    end
-  end
+  # def show
+  #   @question = Question.find(params[:id])
+  #   end
+  # end
 
-  def new
-    @question = Question.new
-    2.times { @question.options << Option.new }
-  end
+  # def new
+  #   @question = Question.new
+  #   2.times { @question.options << Option.new }
+  # end
 
-  def create
-    @question = Question.new(question_params)
-    if @question.save
-      redirect_to @question, notice: "Question #{@question.id} was successfully created."
-    else
-      render :new
-    end
-  end
+  # def create
+  #   @question = Question.new(question_params)
+  #   if @question.save
+  #     redirect_to @question, notice: "Question #{@question.id} was successfully created."
+  #   else
+  #     render :new
+  #   end
+  # end
 
-  def edit
-  end
+  # def edit
+  # end
 
-  def update
-  end
+  # def update
+  # end
 
-  def destroy
-  end
+  # def destroy
+  # end
 
-  private
+  # private
 
-  def question_params
-    params.require(:question).permit(:question, options_attributes: [:id, :answer, :explanation, :correct, :_destroy])
-  end
+  # def question_params
+  #   params.require(:question).permit(:question, options_attributes: [:id, :answer, :explanation, :correct, :_destroy])
+  # end
 end
