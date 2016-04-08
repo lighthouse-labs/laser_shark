c22cf81c812a9742b78132c928bd4e694e118471
Paul Iogna <paul.iogna@gmail.com>
2016-04-08 13:13:51 -0400

    set initial to true on first quiz_submission create

--- app/controllers/quiz_submissions_controller.rb
+++ app/controllers/quiz_submissions_controller.rb
@@ -7,6 +7,7 @@ class QuizSubmissionsController < ApplicationController
   def create
     @quiz_submission = QuizSubmission.new(submission_params)
     @quiz_submission.user = current_user
+    @quiz_submission.initial = previous_submissions? ? false : true
     if @quiz_submission.save
       redirect_to quiz_submission_path @quiz_submission.id
     else
@@ -22,5 +23,9 @@ class QuizSubmissionsController < ApplicationController
   private
   def submission_params
     params.require(:quiz_submission).permit(:quiz_id, answers_attributes: [:option_id])
+  end
+
+  def previous_submissions?
+    QuizSubmission.where("user_id = ? AND quiz_id = ?", current_user.id, @quiz_submission.quiz_id).count > 0
   end
 end
